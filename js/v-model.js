01d322ae8644dc020756d8d865d42951edce76fc
Feng Yu Hua <hannah1234622@gmail.com>
2020-01-03 15:46:17 +0800

    4

---
+++ js/v-model.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        name:'whh',
+        sex:'female',
+        location:[],
+        article:`A sign in attempt requires further verification because we did not recognize your device. To complete the sign in, enter the verification code on the unrecognized device.`,
+        option:2,
+        multiple:[],
+    }
+});
