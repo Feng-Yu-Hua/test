fbcfb037ea01f14433282260aa773be379c6cf51
Feng Yu Hua <hannah1234622@gmail.com>
2020-01-08 15:58:00 +0800

    section

---
+++ js/自我介紹.js
@@ -0,0 +1,12 @@
+new Vue({
+    el:"#app",
+    data:{
+        Home:'首頁',
+        Feature:'自我介紹',
+        Works:'作品',
+        yourname:'你的名字',
+        submit:'輸入',
+        username:'',
+    }
+});
+
